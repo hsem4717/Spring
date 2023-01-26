4670af269c91bd34ff9ac3fba4229cab3a356658
hsem4717 <hsem4717@gmail.com>
2023-01-26 21:39:43 +0900

    ADD::오류수정

---
+++ login/src/main/java/com/example/demo/login/dao/MemberDAO.java
@@ -0,0 +1,7 @@
+package com.example.demo.login.dao;
+
+import com.example.demo.login.dto.MemberDTO;
+
+public interface MemberDAO {
+    public String loginCheck(MemberDTO dto);
+}
---
+++ login/src/main/java/com/example/demo/login/dao/MemberDAOImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.login.dao;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.apache.ibatis.session.SqlSession;
+
+
+@Repository
+public class MemberDAOImpl {
+    @Autowired
+    SqlSession
+
+}
